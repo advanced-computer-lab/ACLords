0137589d256dc33d0b931fe5b26936eb579955ed
kareem <kareemyousef363@gmail.com>
2021-10-30 11:55:51 +0200

    routes and component created

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const mongoose = require('mongoose');
 
@@ -5,9 +6,42 @@ const app = express();
 const port = process.env.PORT || "8000";
 const User = require('./Models/Users');
 
-mongoose.connect("mongodb+srv://ACLords:1234qwer@airlinedb.e4p3f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://ACLords:1234qwer@airlinedb.e4p3f.mongodb.net/AirLineDB?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
 .then(result =>console.log("MongoDB is now connected") )
 .catch(err => console.log(err));
 
+app.set('view engine', 'ejs');
+
+const admin = new User({Name: 'Adminstrator',  
+Email:"admin@gmail.com" ,
+Age: 22 ,
+BornIn: "Admin",
+LivesIn: "Admin", 
+MartialStatus:"Admin",
+PhoneNumber: "0101111002",
+Job: "Admin"
+} , { timestamps: true });
+
+admin.save();
 
 
+
+app.get("/createstudent",(req , res)  =>{
+    res.send(admin)
+  });
+
+  app.get("/Trial",(req , res)  =>{
+    res.render('Trial')
+  });
+  
+  app.get("/Trial2",(req , res)  =>{
+    res.render('Trial2')
+  });
+
+  app.get("/",(req , res)  =>{
+    res.send("hello")
+  });
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
