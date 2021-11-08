08428dca480c7617d8d7dab8ad680773abaf367f
Omar Khalid <o.khalid44@gmail.com>
2021-11-08 11:12:26 +0200

    routes

--- src/frontendapp/src/App.js
+++ src/frontendapp/src/App.js
@@ -3,15 +3,27 @@ import CreateFlight from './Component/CreateFlight'
 import Search from './Component/Search'
 import UpdateFlights from './Component/UpdateFlights'
 import ViewFlights from './Component/ViewFlights'
+import { BrowserRouter, Route, Switch} from "react-router-dom"
 
 export default function App() {
   return (
-    <>
-      {/* <CreateFlight/> */}
-      <ViewFlights/>
-       {/* <Search/> */}
-       {/* <UpdateFlights/> */}
-    </>
+<BrowserRouter>
+   <Switch>
+     <Route path="/CreateFlight" component={CreateFlight} exact/>
+     <Route path="/ViewFlights" component={ViewFlights} exact/>
+     <Route path="/Search" component={Search} exact/>
+
+   </Switch>
+</BrowserRouter>
+
+    // <>
+    //   <CreateFlight/> 
+    //   <ViewFlights/>
+    //    {/* <Search/> */}
+    //    {/* <UpdateFlights/> */}
+    // </>
     
   )
 }
+
+
