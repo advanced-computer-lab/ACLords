77475ebff7a764fd09b5188e0a988b391657b8d3
Omar Khalid <o.khalid44@gmail.com>
2021-12-23 15:01:14 +0200

    still here

--- src/AuthServer.js
+++ src/AuthServer.js
@@ -18,7 +18,7 @@ const bcrypt = require('bcrypt')
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
 const storeItems = new Map([
-  [1,{priceInCents:10000,name:"Flight Payment"}],
+  [1, { priceInCents: 10000, name: "Flight Payment" }],
 ])
 
 app.post("/create-checkout-session", async (req, res) => {
@@ -34,7 +34,7 @@ app.post("/create-checkout-session", async (req, res) => {
             product_data: {
               name: storeItem.name,
             },
-            unit_amount: storeItem.priceInCents ,
+            unit_amount: storeItem.priceInCents,
           },
           quantity: item.quantity,
         }
@@ -50,98 +50,95 @@ app.post("/create-checkout-session", async (req, res) => {
 
 
 app.use(bodyParser.urlencoded({
-    extended: true
-  }))
-  app.use(bodyParser.urlencoded({
-    type: 'application/*+json'
-  }))
-  app.use(bodyParser.json());
-  
-  
-  mongoose.connect("mongodb+srv://ACLords:1234qwer@airlinedb.e4p3f.mongodb.net/AirLineDB?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(result => console.log("MongoDB is now connected"))
-    .catch(err => console.log(err));
-  
-  app.use(cors());
+  extended: true
+}))
+app.use(bodyParser.urlencoded({
+  type: 'application/*+json'
+}))
+app.use(bodyParser.json());
 
 
-let refreshTokens= []
+mongoose.connect("mongodb+srv://ACLords:1234qwer@airlinedb.e4p3f.mongodb.net/AirLineDB?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => console.log("MongoDB is now connected"))
+  .catch(err => console.log(err));
 
-app.post('/token', (req,res) => {
-    const refreshToken = req.body.token
-    if(refreshToken ==null) return res.sendStatus(401)
-    if(refreshTokens.includes(refreshToken)) return res.sendStatus(403)
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) =>
-    {
-        if(err) return res.sendStatus(403)
-        const accessToken = generateAccessToken({email: user.email})
-        res.json({accessToken: accessToken})
-    })
+app.use(cors());
+
+
+let refreshTokens = []
+
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token
+  if (refreshToken == null) return res.sendStatus(401)
+  if (refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    const accessToken = generateAccessToken({ email: user.email })
+    res.json({ accessToken: accessToken })
+  })
 })
 
-app.delete('/logout', (req, res)=>{
-    refreshTokens= refreshTokens.filter(token => token !== req.body.token)
-    res.sendStatus(204)
+app.delete('/logout', (req, res) => {
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+  res.sendStatus(204)
 })
 // app.post("/SignUp", userController.addUser)
 
 app.post('/SignUp', (req, res) => {
-    console.log(req.body);
-    userController.addUser(req.body, res);
+  console.log(req.body);
+  userController.addUser(req.body, res);
 }
 )
 
 
 
-app.post('/login', async (req, res) =>{
-  
-    User.find({Email: req.body.Email})
-  //   console.log("t7t")
-  // console.log(req.body.Email)
+app.post('/login', async (req, res) => {
 
-  // console.log("fo2")
-  .then(async result => {
-    if(result[0] == null){
-      res.send('Email does not exist. Please create new account.')
-    }
-    if(await bcrypt.compare(req.body.Password, result[0].Password)){
-      res.send(result[0])
-    }
-    else{
-      res.send('Password is incorrect')
-    }
-  
-  }).catch(err => {
-    console.log(err);
-  });
-   
-   
-
-    const accessToken= generateAccessToken(user)
-    const refreshToken= jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-    refreshTokens.push(refreshToken)
-    res.json(
-        {
-            accessToken: accessToken, 
+  User.find({ Email: req.body.Email })
+    
+    .then(async result => {
+      if (result[0] == null) {
+        res.send('Email does not exist. Please create new account.')
+      }
+      if (await bcrypt.compare(req.body.Password, result[0].Password)) {
+        const user = {
+          Email: req.body.Email,
+          Password: req.body.Password
+        }
+        const accessToken = generateAccessToken(user)
+        const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+        refreshTokens.push(refreshToken)
+        res.json(
+          {
+            accessToken: accessToken,
             refreshToken: refreshToken
-        })
-    })
-    
-    function generateAccessToken(user){
-        return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s'})
-    }
+          }) 
+      }
+      else {
+        res.send('Password is incorrect')
+      }
 
-    function authenticateToken(req, res, next){
-      const authHeader = req.headers['authorization'];
-      const token = authHeader && authHeader.split(' ')[1] 
-      if(token == null) return res.sendStatus(401)
-    
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if(err) return res.sendStatus(403)
-        req.user = user;
-        next()
-      })    
-    }
+    }).catch(err => {
+      console.log(err);
+    });
 
-    app.listen(4000)
+})
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
+}
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    req.user = user;
+    next()
+  })
+}
+
+app.listen(4000)
